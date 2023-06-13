b1d861bb2b6023216b18c8c55e3f3c3e3d54e4ac
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-13 22:53:08 +0600

    aro pera

---
+++ odd-sum.js
@@ -0,0 +1,37 @@
+function getSumArray(arr){
+ let sum=0;
+ for (var i=0;i<arr.length;i++){
+  const index=i;
+  const element=arr[index];
+  sum=sum+element;
+  // console.log(sum)
+ }
+
+ return sum;
+
+}
+
+const arr=[1,2,3,4,5,6];
+const result=getSumArray(arr);
+console.log(result)
+
+
+
+function getTheOdd(arr){
+ const oddNUmbers=[];
+ for (var i=0;i<arr.length;i++){
+  const index=i;
+  const element=arr[index];
+ if(element%2!==0){
+   console.log(index,element);
+   oddNUmbers.push(element);
+ }
+ }
+ return oddNUmbers;
+}
+
+const array=[1,2,3,4,5,6];
+const resultOdd=getTheOdd(array);
+console.log(resultOdd );
+const finalRes= getSumArray(resultOdd);
+console.log(finalRes)
---
+++ practice-khela.js
@@ -0,0 +1,43 @@
+function findSumArray(arr){
+ let sum=0;
+ for (var i=0;i<arr.length;i++){
+   const index1=i;
+   // console.log(index1);
+   const element=arr[index1];
+   sum=sum+element;
+   console.log(sum)
+ }
+ return sum;
+ 
+}
+
+const arr=[22,44,66];
+findSumArray(arr);
+
+
+
+
+
+
+
+
+
+
+function getEvenNumbers(array){
+ const arrayEven=[];
+ for (let i=0;i<array.length;i++){
+  const index=i;
+  // console.log(index);
+  const element=array[index];
+  // console.log(index,element);
+  if(element%2==0){
+   arrayEven.push(element);
+  }
+ }
+ console.log(arrayEven)
+}
+
+const arrayNums=[11,22,33,44,55,66];
+getEvenNumbers(arrayNums);
+
+
