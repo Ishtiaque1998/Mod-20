123b7a7939d3e0027fe58653ccef141035d32114
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-13 19:10:34 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,24 @@
+var friends=['abul','babul','rabiul'];
+// console.log(index)
+var pos= friends.indexOf('babul');
+// console.log(pos);
+
+// console.log(friends[2]);
+
+// var i=0;
+// while (i=0,i=10) {
+//  console.log('I love you');
+//  i++;
+// }
+
+// var j=0;
+for (var j=0;j<=20; j++){
+ console.log(j,'I hate you')
+}
+
+
+var i=0;
+while (i<=20){
+console.log('gjgjg')
+i++
+}
---
+++ some-problem.js
@@ -0,0 +1,23 @@
+const inch=84;
+
+const value=inch/12;
+console.log(value ,'feet');
+
+const dadaINches=144;
+const dadaFeet=dadaINches/12;
+console.log(dadaFeet)
+
+const dadiInches=90;
+const dadiFeet=dadiInches/12;
+console.log(dadiFeet)
+
+
+
+
+function findFeet(inches){
+ const feet=inches/12;
+ console.log(feet);
+
+}
+
+findFeet(290)
